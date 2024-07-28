c63b29ba67135b146eca1aa716f5dea224dfd292
Nehasonowal <129983079+Nehasonowal@users.noreply.github.com>
2024-07-28 23:24:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('Hello, React!');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
